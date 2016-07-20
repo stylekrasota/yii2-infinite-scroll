0893ff8665f41fbb59db3ad33b0bdb5fe69e961b
panchenkodv <lexx.klan@mail.ru>
2016-07-20 11:44:56 +0300

    url decode in pagination button

--- InfiniteScrollPager.php
+++ InfiniteScrollPager.php
@@ -57,4 +57,22 @@ class InfiniteScrollPager extends LinkPager
         $js = "$('{$this->containerSelector}').infinitescroll({$options});";
         $this->view->registerJs($js);
     }
+
+    protected function renderPageButton($label, $page, $class, $disabled, $active)
+    {
+        $options = ['class' => $class === '' ? null : $class];
+        if ($active) {
+            Html::addCssClass($options, $this->activePageCssClass);
+            return Html::tag('li', Html::tag('span', $label), $options);
+        }
+        if ($disabled) {
+            Html::addCssClass($options, $this->disabledPageCssClass);
+
+            return Html::tag('li', Html::tag('span', $label), $options);
+        }
+        $linkOptions = $this->linkOptions;
+        $linkOptions['data-page'] = $page;
+
+        return Html::tag('li', Html::a($label, urldecode($this->pagination->createUrl($page)), $linkOptions), $options);
+    }
 }
